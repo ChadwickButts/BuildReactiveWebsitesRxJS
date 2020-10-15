013f92a7051d69a2ae6f82c3de4b94a66883ddfb
Chadwick Butts <ccbutts@mail.usf.edu>
2020-10-14 21:35:47 -0400

    Chapter 2 progress

--- code/vanilla/manipulatingStreams/pigLatin.ts
+++ code/vanilla/manipulatingStreams/pigLatin.ts
@@ -1,2 +1,31 @@
+import { from, fromEvent } from "rxjs";
+import { map, mergeMap, reduce, tap } from "rxjs/operators";
+
 let textbox = <HTMLElement>document.querySelector('#text-input');
 let results = <HTMLElement>document.querySelector('#results');
+
+function pigLatinify(word) {
+    if (word.length < 2) {
+        return word;
+    }  
+    return word.slice(1) + '-' + word[0].toLowerCase() + 'ay';
+}
+
+let keyUp$ = fromEvent(textbox, 'keyup').pipe(
+    map(event => event.target['value']),
+    mergeMap(wordString => from(
+            wordString.split(/\s+/)
+        ).pipe(
+            tap(console.log),
+            map(pigLatinify),
+            reduce((bigString, newWord) => bigString + ' ' + newWord, '')
+        )
+    )
+);
+keyUp$.subscribe(translated => console.log(translated));
+
+// Reduce example
+
+from([1,2,3]).pipe(
+    reduce((accumulator, value) => accumulator + value, 0)
+).subscribe(console.log);
--- code/vanilla/sandbox/sandbox.ts
+++ code/vanilla/sandbox/sandbox.ts
@@ -1,2 +1,13 @@
-import { } from 'rxjs';
-import { } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+
+interval(1000)
+​ 	.pipe(
+​ 	  tap(val => {
+​ 	    console.log(​'inside tap'​, val);
+​ 	    ​// This return doesn't change the final value​
+​ 	    ​return​ val * 100;
+​ 	  })
+​ 	)
+​ 	.subscribe(val => console.log(​'inside subscribe'​, val));
